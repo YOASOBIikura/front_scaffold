22cfe4b7b871511702e6f747ca6dcf97c192b4c5
wangzian <723692634@qq.com>
2024-04-19 11:09:30 +0800

    细化拆分子组建

---
+++ src/components/optionReview/description.vue
@@ -0,0 +1,94 @@
+<template>
+           <div class="description">
+                <div class="title">Product Description</div>
+                <div class="product-img">
+                    <div class="absolute-data">
+                        <div class="you">You</div>
+                        <div class="writer">Call Option Writer</div>
+                        <div class="premium">Pay Premium</div>
+                        <div class="pay-amount">10 <img src="@/assets/images/usdt.png" /></div>
+                        <div class="lock">Lock ETH</div>
+                        <div class="lock-amount">1 <img src="@/assets/images/eth.png"/></div>
+                    </div>
+                    <img src="@/assets/images/product.png" />
+                </div>
+            </div>
+
+</template>
+<script setup>
+
+</script>
+<style scoped lang="less">
+.title{
+            font-size: 24px;
+            font-weight: bold;
+}
+ .description{
+        margin-top: 24px;
+        .product-img{
+            margin-top: 24px;
+            height: 272px;
+            position: relative;
+            .absolute-data{
+                width: 100%;
+                height: 100%;
+                position: absolute;
+                font-size: 12px;
+                .you{
+                    position: absolute;
+                    top: 16px;
+                    left: 25%;
+                    transform: translate(-50%);
+                }
+                .writer{
+                    position: absolute;
+                    top: 16px;
+                    right: 25%;
+                    transform: translate(50%);
+                }
+                .premium{
+                    position: absolute;
+                    top: 23%;
+                    left: 50%;
+                    transform: translate(-50%);
+                    background-color: var(--text-color-primary);
+                    color: var( --text-color-active);
+                    padding: 2px 4px;
+                    border-radius: 4px;
+                }
+                .pay-amount{
+                    position: absolute;
+                    top: 36%;
+                    left: 50%;
+                    transform: translate(-50%);
+                      line-height: 12px;
+                    img{
+                        width: 12px;
+                        vertical-align: bottom;
+
+                    }
+                }
+                .lock{
+                    position: absolute;
+                    top: 53%;
+                    right: 18%;
+                    background-color: var(--text-color-primary);
+                    color: var( --text-color-active);
+                    padding: 2px 4px;
+                    border-radius: 4px;
+                }
+                .lock-amount{
+                    position: absolute;
+                    top: 62%;
+                    right: 18%;
+                    transform: translate(-50%);
+                    line-height: 12px;
+                    img{
+                        width: 12px;
+                        vertical-align: bottom;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/optionReview/repay-type.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="repay-type-content">
+        <div class="item">
+            <img src="@/assets/images/settlement_icon_1.png"/>
+            <div class="data">
+                <div class="item-title">Exercise for asset delivery</div>
+                <div class="item-info">Pay 3,100 USDT to get 1 ETH</div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="@/assets/images/settlement_icon_2.png"/>
+            <div class="data">
+                <div class="item-title">Exercise for cash settlement</div>
+                <div class="item-info">Get net profit in ETH</div>
+            </div>
+        </div>
+        <div class="item">
+                <img src="@/assets/images/settlement_icon_3.png"/>
+            <div class="data">
+                <div class="item-title">Not exercise</div>
+                <div class="item-info">No payment needed</div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped lang="less">
+.title{
+    font-size: 24px;
+    font-weight: bold;
+}
+ .repay-type-content{
+        margin-top: 44px;
+        width: 100%;
+        padding: 24px 16px;
+        border: 1px solid var(--component-border);
+        border-radius: 16px;
+        margin-bottom: 60px;
+        .item{
+            margin-top: 24px;
+            display: flex;
+            font-size: 14px;
+            img{
+                width: 40px;
+            }
+            .data{
+                margin-left: 12px;
+                .item-title{
+                    color: var(--text-color-second);
+                }
+                
+            }
+            
+            &:first-child{
+                margin-top: 0;
+            }
+        }
+    }
+</style>
