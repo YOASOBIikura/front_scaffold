4e2780a62c2af71391b0d718afe5aa9b1690e266
wangzian <723692634@qq.com>
2024-04-30 18:02:29 +0800

    解决BUG

--- src/api/protfolio.js
+++ src/api/protfolio.js
@@ -1,6 +1,6 @@
 import { useAxiosStore } from "@/pinia/modules/axios";
 
-function getMyOfferApi(_optionId,_chainId,_wallet, _page = 1, _pageSize = 10){
+function getOrderApi(_optionId,_chainId,_wallet, _page = 1, _pageSize = 10){
     return useAxiosStore().axios({
         mode:"http", //chainBlockCall  http  chainBlockSend  sign  unSign  
         target:"/option/offer_list" ,// chainBlock 时是合约地址   http时是url
